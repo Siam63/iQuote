f12ac36b3688e126824937445b251ef7e6e3675f
siamr <siam_1000@hotmail.com>
2022-11-15 14:07:30 -0500

    Initial Commit

---
+++ iQuote/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iQuote
+//
+//  Created by Siam Rahman on 2022-11-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iQuote/iQuoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  iQuoteApp.swift
+//  iQuote
+//
+//  Created by Siam Rahman on 2022-11-15.
+//
+
+import SwiftUI
+
+@main
+struct iQuoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
